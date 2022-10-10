b4f3cb19b2238cfc70da20c2c10c0eb0923850c4
Suleym <suleymevlo81@gmail.com>
2022-10-10 13:29:36 +0300

    Урок 24. Подключил CSS файл

---
+++ src/lesson24/App.js
@@ -0,0 +1,14 @@
+import Block from "./components/Block";
+
+function App() {
+    return (
+        <div className='App'>
+            <h1 style={{color: 'red', fontSize: 49}}>Hello world</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, quos.</p>
+
+            <Block/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/lesson24/components/Block/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Block.css'
+
+const Block = () => {
+  return (
+    <div className='block'></div>
+  )
+}
+
+
+export default Block;
