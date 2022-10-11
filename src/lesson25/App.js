79d7e9645ee295083a289f4af1dfc3affb9d2ebe
Suleym <suleymevlo81@gmail.com>
2022-10-11 11:19:52 +0300

    Урок 26. Использовал CSS Modules

--- src/lesson25/App.js
+++ src/lesson25/App.js
@@ -1,17 +1,12 @@
 import Block from "./components/Block";
+import Article from './components/Article';
 import './index.scss'
 function App() {
     return (
         <div className="App">
             <Block />
-            <div className="article">
-                <h2>Это важная новость!</h2>
-                <p>
-                    Lorem ipsum dolor sit <b>amet</b> consectetur adipisicing elit.
-                    Architecto, quos.
-                    <button>Читать далее</button>
-                </p>
-            </div>
+            <Article/>
+            <button className="redButton">Красная кнопка</button>
         </div>
     );
 }
